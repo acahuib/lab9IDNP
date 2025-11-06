c91c5cbc77b6d4d9486ced2379d56e86b1c59288
Anthony <acahuib@unsa.edu.pe>
2025-11-06 12:52:29 -0500

    Se agrega la forma de poder ver el texto de preparacion en pantalla

--- app/src/main/java/com/example/lab9/ui/RecetaAdapter.kt
+++ app/src/main/java/com/example/lab9/ui/RecetaAdapter.kt
@@ -14,6 +14,7 @@ class RecetaAdapter(private val lista: List<Receta>) :
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val nombre: TextView = v.findViewById(R.id.txtNombre)
         val ingredientes: TextView = v.findViewById(R.id.txtIngredientes)
+        val preparacion: TextView = v.findViewById(R.id.txtPasos)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,8 +25,9 @@ class RecetaAdapter(private val lista: List<Receta>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val receta = lista[position]
-        holder.nombre.text = receta.nombre
-        holder.ingredientes.text = receta.ingredientes
+        holder.nombre.text = "Nombre: ${receta.nombre}"
+        holder.ingredientes.text = "Ingredientes: ${receta.ingredientes}"
+        holder.preparacion.text = "Preparación: ${receta.pasos}"
     }
 
     override fun getItemCount() = lista.size
